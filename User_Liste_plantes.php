d19afe4982e89c9949140f0fb3466888302b0564
jjuulliieee <julie.hoegy607@gmail.com>
2024-11-01 21:56:43 +0100

    accès à la liste des plantes (user)

---
+++ User_Liste_plantes.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Liste de toutes les plantes</title>
+</head>
+<body>
+
+    <div class="liste_plantes">
+        <h2>Liste de toutes les plantes</h2>
+        <p>Cliquez sur son nom pour accéder à sa description et son prix</p>
+        <br>
+        <?php
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+
+        //On accède à la base de donnée
+        require_once 'bdd.php';
+
+        // Requête SQL pour sélectionner et afficher une colonne
+        $sql = "SELECT nom, generique FROM plante";
+        $req = $connexion->query($sql);
+
+        while($rep = $req->fetch()){
+            echo "<p><a href='User_Plante_read.php?plante=" . urlencode($rep['generique']) . "'>" . htmlspecialchars($rep['nom']) . " (" . htmlspecialchars($rep['generique']) . ")</a></p><br>";}
+
+        ?>
+    </div>
+
+
+</body>
+</html>
+
+
+
